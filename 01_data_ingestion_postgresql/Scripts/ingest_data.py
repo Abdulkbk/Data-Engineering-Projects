95900dfada7da7ff5d78dbed0c8e5e8b3e5daefb
Abdulkbk <abdoollahikbk@gmail.com>
2022-07-23 11:47:12 +0100

    Data ingestion with python into a running postgres instance that is running locally

---
+++ 01_data_ingestion_postgresql/Scripts/ingest_data.py
@@ -0,0 +1,54 @@
+from email import iterators
+import imp
+import os
+import pandas as pd
+import argparse
+from sqlalchemy import create_engine
+
+def main(params):
+  user = params.user
+  password = params.password
+  host = params.host
+  port = params.port
+  db = params.db
+  url = params.url
+  table_name = params.table_name
+
+  file_output = 'output.parquet'
+
+  file_csv = 'output_data.csv'
+
+  """Downloading the parquet file"""
+  os.system(f'curl {url} -o data/{file_output}')
+
+  """Creating an engine connection to postgresql database"""
+  engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+  df_parquet = pd.read_parquet(f'data/{file_output}')
+
+  df_parquet.to_csv(f'data/{file_csv}', index=False, sep="\t")
+
+  df_iter = pd.read_csv(f'data/{file_csv}', iterator=True, chunksize=100000)
+
+  df = next(df_iter)
+
+  df.head(0).to_sql(name='yellow_taxi', con=engine, if_exists='replace')
+
+  df.to_sql(name=f'{table_name}', con=engine, if_exists='append')
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Ingest parquet file to postgres db running on docker")
+
+  parser.add_argument('--user', help='username for db')
+  parser.add_argument('--password', help='password for db')
+  parser.add_argument('--host', help='host for db')
+  parser.add_argument('--port', help='port for db')
+  parser.add_argument('--db', help='db name')
+  parser.add_argument('--table-name', help='table name')
+  parser.add_argument('--url', help='url of the csv file')
+
+  args = parser.parse_args()
+
+  main(args)
+
+
